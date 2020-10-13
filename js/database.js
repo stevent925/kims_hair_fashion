f28aba8ae4921df9b002c479c5f2e15e05f79bbe
Steven Tran <stevent925@gmail.com>
2020-10-13 11:39:44 -0500

    Update database.js

--- js/database.js
+++ js/database.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var conn = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "earwax925",
+    password: "",
     database: 'kims_hair_fashion'
 });
 
